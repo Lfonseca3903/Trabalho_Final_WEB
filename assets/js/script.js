ad2a9844c254f7fafc5246bb5fe45153acaf6f7b
Rúben Pereira <rubengppereira@gmail.com>
2025-01-14 16:48:50 +0000

    Design and JS Improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,48 @@
+// Arrays com os caminhos das imagens
+const retratos = ["./assets/img/retrato1.jpg", "./assets/img/retrato2.jpg", "./assets/img/retrato3.jpg", "./assets/img/retrato4.jpg", "./assets/img/retrato5.jpg"];
 
-function showSection(sectionId) {
-    
-    document.querySelectorAll("div[id]").forEach((section) => {
-      section.style.display = "none";
-    });
-  
-    
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.style.display = "block";
-    }
+const paisagens = ["./assets/img/paisagem1.jpg", "./assets/img/paisagem2.jpg", "./assets/img/paisagem3.jpg", "./assets/img/paisagem4.jpg", "./assets/img/paisagem5.jpg"];
+
+const concertos = ["./assets/img/concerto1.jpg", "./assets/img/concerto2.jpg", "./assets/img/concerto3.jpg", "./assets/img/concerto4.jpg", "./assets/img/concerto5.jpg"];
+
+const desportos = ["./assets/img/desporto1.jpg", "./assets/img/desporto2.jpg", "./assets/img/desporto3.jpg", "./assets/img/desporto4.jpg", "./assets/img/desporto5.jpg"];
+
+//Função que determina qual array de imagens usar
+function mostrarGaleria(galeria) {
+
+  // Seleciona o container do carousel
+  const carouselInner = document.querySelector(
+    "#carouselExampleIndicators .carousel-inner"
+  );
+
+  // Define qual array usar
+  let imagensSel = [];
+  if (galeria === "retratos") {
+    imagensSel = retratos;
+  } else if (galeria === "paisagens") {
+    imagensSel = paisagens;
+  } else if (galeria === "concertos") {
+    imagensSel = concertos;
+  } else if (galeria === "desportos") {
+    imagensSel = desportos;
   }
+
+  // Limpa o carousel atual
+  carouselInner.innerHTML = "";
+
+  // Cria slides para cada imagem
+  imagensSel.forEach((src, index) => {
+    const carouselItem = document.createElement("div");
+    carouselItem.classList.add("carousel-item");
+    if (index === 0) {
+      carouselItem.classList.add("active");
+    }
+
+    const img = document.createElement("img");
+    img.src = src;
+    img.classList.add("d-block", "w-100", "h-100");
+
+    carouselItem.appendChild(img);
+    carouselInner.appendChild(carouselItem);
+  });
+}
