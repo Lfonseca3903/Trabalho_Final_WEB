8b5f14cf98f1f478ba4419f1cc8690f7f1b18f72
Rúben Pereira <rubengppereira@gmail.com>
2025-01-23 00:27:34 +0000

    Trabalho final

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('submitButton').addEventListener('click', function () {
+    const successModal = new bootstrap.Modal(document.getElementById('successModal'));
+    successModal.show();
+});
